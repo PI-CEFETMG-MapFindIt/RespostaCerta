cef00753ab54d7bdc1c8fc6c761de54633027648
Adalberto Barbosa Vieira <adalbertob.vieira@gmail.com>
2017-05-19 11:59:59 -0300

    implementação de alguns métdos

---
+++ RespostaCertaPersistenceRAM/src/br/cefetmg/respostaCerta/model/dao/ClosedQuestionDAOImpl.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.respostaCerta.model.dao;
+
+import br.cefetmg.respostaCerta.model.domain.ClosedQuestion;
+import br.cefetmg.respostaCerta.model.exception.PersistenceException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author umcan
+ */
+public class ClosedQuestionDAOImpl implements ClosedQuestionDAO{
+    private static ClosedQuestionDAOImpl closedDAO = null;        
+
+    private static final HashMap<Long, ClosedQuestion> closedQuestionDB = new HashMap<>();    
+    private static long closedCount;
+    
+    /**
+     *
+     */
+    public ClosedQuestionDAOImpl() { 
+        closedCount = 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static ClosedQuestionDAOImpl getInstance() {
+        
+        if (closedDAO == null)
+            closedDAO = new ClosedQuestionDAOImpl();
+        
+        return  closedDAO;
+    }
+    
+    /**
+     *
+     * @param questaoFechada
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public void insert(ClosedQuestion questaoFechada) throws PersistenceException {
+
+        if (questaoFechada == null)
+            throw new PersistenceException("Entidade não pode ser nula.");                
+        
+        Long questId = questaoFechada.getIdQuestao();
+        
+        if ((questId != null) && closedQuestionDB.containsKey(questId))
+            throw new PersistenceException("Duplicação de chave.");
+        
+        questId = ++closedCount;
+        questaoFechada.setIdQuestao(questId);
+        closedQuestionDB.put(questId, questaoFechada);
+    }
+    
+    /**
+     *
+     * @param closedQuestion
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public void update(ClosedQuestion closedQuestion) throws PersistenceException {
+
+        if (closedQuestion == null)
+            throw new PersistenceException("Entidade não pode ser nula.");              
+        
+        Long closedId = closedQuestion.getIdQuestao();
+
+        if (closedId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");        
+        
+        if (!closedQuestionDB.containsKey(closedId))
+            throw new PersistenceException("Não existe entidade com a chave " + closedId + ".");
+        
+        closedQuestionDB.replace(closedId, closedQuestion);
+    }
+
+    /**
+     *
+     * @param closedId
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public ClosedQuestion delete(Long closedId) throws PersistenceException {
+        if (closedId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");
+        
+        if (!closedQuestionDB.containsKey(closedId))
+            throw new PersistenceException("Não existe entidade com a chave " + closedId + ".");
+        
+        return closedQuestionDB.remove(closedId);
+    }
+
+    /**
+     *
+     * @param closedId
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    public ClosedQuestion getClosedQuestionById(Long closedId) throws PersistenceException {
+        
+        if (closedId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");
+        
+        if (!closedQuestionDB.containsKey(closedId))
+            throw new PersistenceException("Não existe entidade com a chave " + closedId + ".");
+        
+        return closedQuestionDB.get(closedId);        
+        
+    }
+
+    /**
+     *
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    public List<ClosedQuestion> listAll() throws PersistenceException {
+        List<ClosedQuestion> closedList = new ArrayList<>();
+        
+        Iterator<ClosedQuestion> iterator = closedQuestionDB.values().iterator();
+	while (iterator.hasNext())
+            closedList.add(iterator.next());
+        
+        return closedList;
+    }
+}
---
+++ RespostaCertaPersistenceRAM/src/br/cefetmg/respostaCerta/model/dao/ForumDAOImpl.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.respostaCerta.model.dao;
+
+import br.cefetmg.respostaCerta.model.domain.Forum;
+import br.cefetmg.respostaCerta.model.domain.Forum;
+import br.cefetmg.respostaCerta.model.exception.PersistenceException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author umcan
+ */
+public class ForumDAOImpl implements ForumDAO{
+    
+    private static ForumDAOImpl forumDAO = null;        
+
+    private static final HashMap<Long, Forum> forumDB = new HashMap<>();    
+    private static long forumCount;
+    
+    public ForumDAOImpl() { 
+        forumCount = 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static ForumDAOImpl getInstance() {
+        
+        if (forumDAO == null)
+            forumDAO = new ForumDAOImpl();
+        
+        return  forumDAO;
+    }
+    
+    /**
+     *
+     * @param forum
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public void insert(Forum forum) throws PersistenceException {
+
+        if (forum == null)
+            throw new PersistenceException("Entidade não pode ser nula.");                
+        
+        Long forumId = forum.getQuestao().getIdQuestao();
+        
+        if ((forumId != null) && forumDB.containsKey(forumId))
+            throw new PersistenceException("Duplicação de chave.");
+        
+        forumId = ++forumCount;
+        forum.getQuestao().setIdQuestao(forumId);
+        forumDB.put(forumId, forum);
+    }
+    
+    /**
+     *
+     * @param forum
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public void update(Forum forum) throws PersistenceException {
+
+        if (forum == null)
+            throw new PersistenceException("Entidade não pode ser nula.");              
+        
+        Long forumId = forum.getQuestao().getIdQuestao();
+
+        if (forumId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");        
+        
+        if (!forumDB.containsKey(forumId))
+            throw new PersistenceException("Não existe entidade com a chave " + forumId + ".");
+        
+        forumDB.replace(forumId, forum);
+    }
+
+    /**
+     *
+     * @param forumId
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    synchronized public Forum delete(Long forumId) throws PersistenceException {
+        if (forumId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");
+        
+        if (!forumDB.containsKey(forumId))
+            throw new PersistenceException("Não existe entidade com a chave " + forumId + ".");
+        
+        return forumDB.remove(forumId);
+    }
+
+    /**
+     *
+     * @param forumId
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    public Forum getForumById(Long forumId) throws PersistenceException {
+        
+        if (forumId == null)
+            throw new PersistenceException("Chave da entidade não pode ser nulo.");
+        
+        if (!forumDB.containsKey(forumId))
+            throw new PersistenceException("Não existe entidade com a chave " + forumId + ".");
+        
+        return forumDB.get(forumId);        
+        
+    }
+
+    /**
+     *
+     * @return
+     * @throws PersistenceException
+     */
+    @Override
+    public List<Forum> listAll() throws PersistenceException {
+        List<Forum> forumList = new ArrayList<>();
+        
+        Iterator<Forum> iterator = forumDB.values().iterator();
+	while (iterator.hasNext())
+            forumList.add(iterator.next());
+        
+        return forumList;
+    }
+    
+}
