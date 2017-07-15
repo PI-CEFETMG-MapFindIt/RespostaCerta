c5425ebca3cfbe4ea0b57bfec2d1400c90cd7a4e
Adalberto Barbosa Vieira <adalbertob.vieira@gmail.com>
2017-07-15 01:25:02 -0300

    correções MVC

--- RespostaCertaWeb/src/java/br/cefetmg/respostaCerta/controller/PagQuestaoFechada.java
+++ RespostaCertaWeb/src/java/br/cefetmg/respostaCerta/controller/PagQuestaoFechada.java
@@ -6,11 +6,15 @@
 package br.cefetmg.respostaCerta.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import br.cefetmg.respostaCerta.model.domain.ClosedQuestion;
-import br.cefetmg.respostaCerta.model.dao.ClosedQuestionDAO;
+import br.cefetmg.respostaCerta.model.service.ClosedQuestionManagement;
+import br.cefetmg.respostaCerta.model.service.ClosedQuestionManagementImpl;
 import br.cefetmg.respostaCerta.model.dao.ClosedQuestionDAOImpl;
+import br.cefetmg.respostaCerta.model.domain.ClosedQuestion;
 import br.cefetmg.respostaCerta.model.domain.Question;
+import br.cefetmg.respostaCerta.model.exception.BusinessException;
 import br.cefetmg.respostaCerta.model.exception.PersistenceException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,11 +24,11 @@ public class PagQuestaoFechada {
     public static String processa(HttpServletRequest request){
         try{
             Question question = (Question) request.getAttribute("questao");
-            ClosedQuestionDAO questionDAO = new ClosedQuestionDAOImpl();
-            ClosedQuestion closedQuestion = questionDAO.getClosedQuestionById(question.getIdQuestao());
+            ClosedQuestionManagement questionDAO = new ClosedQuestionManagementImpl(new ClosedQuestionDAOImpl());
+            ClosedQuestion closedQuestion = questionDAO.getQuestionById(question.getIdQuestao());
             request.setAttribute("question", closedQuestion);
             return "ResponderQuestaoFechada.jsp";
-        }catch (PersistenceException ex) {
+        }catch (PersistenceException | BusinessException  ex) {
             request.setAttribute("erro", ex.getMessage());
             return "Erro.jsp";
         }      
