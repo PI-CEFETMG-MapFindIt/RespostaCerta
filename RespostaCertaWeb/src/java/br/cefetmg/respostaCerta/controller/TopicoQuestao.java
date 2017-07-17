0c4577c64d4a0d0ff6a4f52df7c8ed5fed077975
vitorricoy <vitorricoy@yahoo.com.br>
2017-07-17 17:55:21 -0300

    Fix Topic

--- RespostaCertaWeb/src/java/br/cefetmg/respostaCerta/controller/TopicoQuestao.java
+++ RespostaCertaWeb/src/java/br/cefetmg/respostaCerta/controller/TopicoQuestao.java
@@ -1,5 +1,6 @@
 package br.cefetmg.respostaCerta.controller;
 
+import br.cefetmg.respostaCerta.model.dao.ClosedQuestionDAOImpl;
 import br.cefetmg.respostaCerta.model.dao.OpenQuestionDAOImpl;
 import br.cefetmg.respostaCerta.model.dao.TopicAnswerDAOImpl;
 import br.cefetmg.respostaCerta.model.dao.TopicDAOImpl;
@@ -8,6 +9,8 @@ import br.cefetmg.respostaCerta.model.domain.Topic;
 import br.cefetmg.respostaCerta.model.domain.TopicAnswer;
 import br.cefetmg.respostaCerta.model.exception.BusinessException;
 import br.cefetmg.respostaCerta.model.exception.PersistenceException;
+import br.cefetmg.respostaCerta.model.service.ClosedQuestionManagement;
+import br.cefetmg.respostaCerta.model.service.ClosedQuestionManagementImpl;
 import br.cefetmg.respostaCerta.model.service.OpenQuestionManagement;
 import br.cefetmg.respostaCerta.model.service.OpenQuestionManagementImpl;
 import br.cefetmg.respostaCerta.model.service.TopicAnswerManagement;
@@ -25,15 +28,13 @@ class TopicoQuestao {
     public static String processa(HttpServletRequest request){
         Long id = Long.parseLong(request.getParameter("id")); 
         TopicManagement tpcMan = new TopicManagementImpl(new TopicDAOImpl());
-        OpenQuestionManagement oqMan = new OpenQuestionManagementImpl(new OpenQuestionDAOImpl());
         TopicAnswerManagement tpcAMan = new TopicAnswerManagementImpl(new TopicAnswerDAOImpl());
         try {
             List<TopicAnswer> r = tpcAMan.getAnswersTopic(id);
             request.setAttribute("respostas", r);
             Topic t = tpcMan.getTopicById(id);
             request.setAttribute("topico", t);
-            Question q = oqMan.getQuestionById(id);
-            request.setAttribute("questao", q);
+            request.setAttribute("questao", t.getForum().getQuestao());
             return "Topico.jsp";
         } catch (BusinessException | PersistenceException ex) {
             request.setAttribute("erro", ex.getMessage());
