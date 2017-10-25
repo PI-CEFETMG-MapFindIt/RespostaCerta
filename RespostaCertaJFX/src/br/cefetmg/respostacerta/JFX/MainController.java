76f174735cd2c0ce01cb9c486812aba2647fd7b1
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2017-10-25 14:48:46 -0200

    Controller JFX configurado

--- RespostaCertaJFX/src/br/cefetmg/respostacerta/JFX/MainController.java
+++ RespostaCertaJFX/src/br/cefetmg/respostacerta/JFX/MainController.java
@@ -1,7 +1,19 @@
 package br.cefetmg.respostacerta.JFX;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class MainController {
-    public void test() {
-        System.out.println("testado");
+    
+    public ChoiceBox<String> choiceBox;
+    
+    public MainController() {
+        choiceBox = new ChoiceBox<>();
+    }
+    
+    @FXML
+    public void initialize() {
+        choiceBox.getItems().addAll("Questão", "Módulo", "Disciplina");
+        choiceBox.setValue("Questão");
     }
 }
