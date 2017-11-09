02816a95788750d15cd55de4e3907fc6cae4a081
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2017-11-09 11:27:32 -0200

    Interface de Login

--- RespostaCertaJFX/src/br/cefetmg/respostacerta/JFX/MainController.java
+++ RespostaCertaJFX/src/br/cefetmg/respostacerta/JFX/MainController.java
@@ -1,15 +1,20 @@
 package br.cefetmg.respostacerta.JFX;
         
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainController {
     
     public ChoiceBox<String> choiceBox;
-    
-    public RespostaCertaJFX mainView;
+    private final FXMLLoader loader;
     
     public MainController() {
-        choiceBox = new ChoiceBox<>();
+        loader = new FXMLLoader();
+        choiceBox = new ChoiceBox<>();  
     }
     
     public void initialize() {
@@ -17,8 +22,17 @@ public class MainController {
         choiceBox.setValue("Questão");
     }
     
-    public void entrar() {
-    
+    public void entrar() throws Exception {
+        loader.setLocation(getClass().getResource("Entrar.fxml"));
+        Parent root = loader.load();
+   
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Login");
+        window.setResizable(false);
+
+        window.setScene(new Scene(root, 500, 200));
+        window.showAndWait();
     }
     
     public void cadastrar() {
